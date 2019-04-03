c2765032b1b82588883202db9bae55720dd2319b
resmithers <rory.smith123@hotmail.co.uk>
2019-04-03 09:28:47 +0100

    Some further basic tests added

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -8,5 +8,13 @@ describe('All Tests', () => {
         it('Successful visit & logic test', () => {
             expect(true).to.eql(true)
         });
+        it('Shows title', () => {
+            cy.get('h1').should('contain', 'HotelAdvisor');
+        });
+        it('Shows hotel cards', () => {
+            cy.get('.row')
+                .find('.card')
+                .should('have.length', 5)
+        });
     });
 });
