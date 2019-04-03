722e4a15e7e7ac08b3999b9e61de90c535e2d519
resmithers <rory.smith123@hotmail.co.uk>
2019-04-03 09:49:11 +0100

    Found simple package to show star ratings

--- src/components/HotelCard.jsx
+++ src/components/HotelCard.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import StarRatings from 'react-star-ratings';
 
 export default ({ data: { name, starRating, facilities } }) => (
 	<Card style={{ height: '20rem', overflowY: 'scroll', marginTop: '10px' }}>
 		<Card.Header>{name}</Card.Header>
-		<Card.Body>({starRating})</Card.Body>
-			{facilities.length > 0 && facilities.map(fac => <p key={fac}>{fac}</p>)}
-			{facilities.length === 0 && <p>No facilities</p>}
+		<Card.Body>
+			<StarRatings
+				rating={starRating}
+				starRatedColor="red"
+				numberOfStars={5}
+				name="rating"
+				starDimension="20px"
+				starSpacing="10px"
+			/>
+			<p>({starRating})</p>
+		</Card.Body>
+		{facilities.length > 0 && facilities.map(fac => <p key={fac}>{fac}</p>)}
+		{facilities.length === 0 && <p>No facilities</p>}
 	</Card>
 );
